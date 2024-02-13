558526d219aaacdeb51daf097153a9c8922934dd
Sammieblz <samuelndubuisi32@gmail.com>
2024-02-13 16:05:30 -0500

    Assignment 1 classes first commit

---
+++ A1_classes/patient_record.cpp
@@ -0,0 +1,10 @@
+/*
+Names: Samuel Ndubuisi and Nivyan Chilumula-Jala 
+
+Date: 2/1/2024
+
+Description: This code provides basic validation for each 
+field and writes the patient records to a file using classes
+*/
+
+
--- A1_functions/patient_record.cpp
+++ A1_functions/patient_record.cpp
@@ -4,7 +4,7 @@ Names: Samuel Ndubuisi and Nivyan Chilumula-Jala
 Date: 2/1/2024
 
 Description: This code provides basic validation for each 
-field and writes the patient records to a file
+field and writes the patient records to a file using functions
 */
 
 #include <iostream>
